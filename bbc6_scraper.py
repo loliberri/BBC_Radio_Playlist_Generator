cba06e65f71410bf77ee2d9a592c3859a6b59abe
loliberri <rebecca.h.hammond@gmail.com>
2026-09-03 21:08:23 +0100

    Add Youtube Authentication

--- bbc6_scraper.py
+++ bbc6_scraper.py
@@ -1,6 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
+import os
+
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
+
+YOUTUBE_SCOPES = [
+    "https://www.googleapis.com/auth/youtube"
+]
+
+
+def get_youtube_service():
+    credentials = Credentials(
+        token=None,
+        refresh_token=os.environ["YOUTUBE_REFRESH_TOKEN"],
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.environ["YOUTUBE_CLIENT_ID"],
+        client_secret=os.environ["YOUTUBE_CLIENT_SECRET"],
+        scopes=YOUTUBE_SCOPES,
+    )
+
+    credentials.refresh(Request())
+
+    return build(
+        "youtube",
+        "v3",
+        credentials=credentials
+    )
+
 # Dictionary of station name -> playlist URL
 STATIONS = {
     "bbc6": "https://onlineradiobox.com/uk/bbcradio6/playlist/",
